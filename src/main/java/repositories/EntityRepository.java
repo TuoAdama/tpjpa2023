4823f91b53e8fc6a0b7f9e521e5650e4321b31cb
Tuo Adama <adama.tuo@etudiant.univ-rennes1.fr>
2023-01-17 11:12:15 +0100

    add: repository

---
+++ src/main/java/repositories/EntityRepository.java
@@ -0,0 +1,11 @@
+package repositories;
+
+import java.util.List;
+
+public interface EntityRepository<T>{
+    void create(T t);
+    void delete(T t);
+    void update(T t);
+    List<T> getAll();
+    T find(long id);
+}
